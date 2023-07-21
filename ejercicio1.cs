0479c965f5b4907c3f32f52075bf308a522fdefa
Akko004 <119001747+Akko004@users.noreply.github.com>
2023-07-21 13:49:47 -0500

    Add files via upload

---
+++ ejercicio1.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("nombre: ");
+string n = Console.ReadLine();
+Console.WriteLine("Apellido: ");
+string a = Console.ReadLine();
+Console.WriteLine("edad: ");
+int e = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Ciudad: ");
+string c = Console.ReadLine();
+
+
+Console.WriteLine("Hola " + n + " " + a);
+Console.WriteLine("Tienes " + e + "años");
+Console.WriteLine("Eres de " + c);
