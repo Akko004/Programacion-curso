0c12b7c136ce4a0bf006187839487c0f19f3f25b
Akko004 <119001747+Akko004@users.noreply.github.com>
2023-07-21 13:05:03 -0500

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,3 +1,13 @@
-﻿Console.WriteLine("Escribir nombre");
-String nombre = Console.ReadLine();
-Console.WriteLine("Hola "  + nombre);
+﻿Console.WriteLine("nombre: ");
+string n = Console.ReadLine();
+Console.WriteLine("Apellido: ");
+string a = Console.ReadLine();
+Console.WriteLine("edad: ");
+int e = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Ciudad: ");
+string c = Console.ReadLine();
+
+
+Console.WriteLine("Hola " + n + " " + a);
+Console.WriteLine("Tienes " + e + "años");
+Console.WriteLine("Eres de " + c);
